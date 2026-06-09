ba22f46da66140fdf13d527ac1df7315c18904b3
Freddy-Baldizon <freddybladizon@gmail.com>
2026-06-08 20:28:07 -0600

    Recipe and usar repository agregados

---
+++ src/RecipeApp.Infrastructure/Repositories/RecipeRepository.cs
@@ -0,0 +1,61 @@
+using Microsoft.EntityFrameworkCore;
+using ProyectoSW4.Domain.Entities;
+
+namespace RecipeApp.Infrastructure.Repositories;
+
+public class RecipeRepository : IRecipeRepository
+{
+    private readonly AppDbContext _context;
+
+    public RecipeRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Recipe>> GetAllAsync()
+    {
+        return await _context.Recipe
+            .Include(r => r.User)
+            .Include(r => r.Country)
+            .ToListAsync();
+    }
+
+    public async Task<Recipe?> GetByIdAsync(int id)
+     {
+            //Devolver solo el id de la receta
+         return await _context.Recipe
+    //         .Include(r => r.User)
+    //         .Include(r => r.Country)
+    //         .Include(r => r.Comments)
+    //         .Include(r => r.Ratings)
+    //         .Include(r => r.Steps)
+    //         .Include(r => r.RecipeIngredients)
+    //         .Include(r => r.RecipeFavorites)
+             .FirstOrDefaultAsync(r => r.Id == id);
+     }
+
+    public async Task<Recipe?> GetByRecipename(string recipeName)
+    {
+        //Devolver solo el id del usuario o dejarlo como esta(DTO)
+         return await _context.Recipe
+        //     .Include(r => r.User)
+        //     .Include(r => r.Country)
+            .FirstOrDefaultAsync(r => r.Name == recipeName);
+    }
+
+    public async Task<Recipe> AddAsync(Recipe recipe)
+    {
+        await _context.Recipe.AddAsync(recipe);
+        return recipe;
+    }
+
+    public async Task DeleteAsync(Recipe recipe)
+    {
+        _context.Recipe.Remove(recipe);
+        await Task.CompletedTask;
+    }
+    
+}
+
+
+
