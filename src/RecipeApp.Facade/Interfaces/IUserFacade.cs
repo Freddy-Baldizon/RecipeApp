460e36e2db46093cfd5dbb0577fe030e8faf3897
Justin Estrada Cruz <justinestradacruz2006@gmail.com>
2026-06-15 18:24:16 -0600

    <Fix> fix merge

--- src/RecipeApp.Facade/Interfaces/IUserFacade.cs
+++ src/RecipeApp.Facade/Interfaces/IUserFacade.cs
@@ -10,6 +10,6 @@ namespace RecipeApp.Facade.Interfaces
         Task<UserDto?> GetByEmailAsync(String email);
         Task<UserDto?> GetByUsername(String username);
         Task DeleteAsync(int userId);
-        Task UpdateAsync(int id, UpdateUserDto userDto);
+        Task<UserDto> UpdateAsync(int id, UpdateUserDto userDto);
     }
 }
